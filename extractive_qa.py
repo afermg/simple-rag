3298f8d6e6c4c3a9e6c12c80d0f8bf7f1089dd42
Alán F. Muñoz <afer.mg@gmail.com>
2024-09-20 11:33:23 -0400

    [WIP]feat: use stream to load extracted text

--- extractive_qa.py
+++ extractive_qa.py
@@ -1,6 +1,5 @@
 from txtai.embeddings import Embeddings
-from txtai.pipeline import Extractor
-from txtai.pipeline import Textractor
+from txtai.pipeline import Extractor, LLM, Textractor
 from pathlib import Path
 from itertools import chain
 
@@ -8,21 +7,17 @@ DATA_PATH = Path("/home/amunoz/projects/txtai/data")
 # Create and run pipeline
 textract = Textractor(paragraphs=True)
 
-# Create extractor instance
-extractor = Extractor(embeddings, "distilbert-base-cased-distilled-squad")
-     
-data = list(chain.from_iterable(extracted))
-embeddings = Embeddings({"content": True})
-embeddings.index(data)
+def stream(path):
+  for f in path.rglob("*"):
+    # Only accept documents
+    if f.suffix in("docx", "xlsx", "pdf"):
+      print(f"Indexing {fpath}")
+      for paragraph in textractor(fpath):
+        yield paragraph
 
-# Create and run pipeline
-rag = RAG(embeddings, "google/flan-t5-base", template="""
-  Answer the following question using the provided context.
+embeddings = Embeddings(content=True)
+embeddings.index(stream(DATA_PATH))
 
-  Question:
-  {question}
-
-  Context:
-  {context}
-""")
-
+llm = LLM(
+    # "ollama/llama3.1:70b"
+          path="ollama/llama3.1:70b", api_base="http://localhost:11434")
